899d632a9c5da6e61cc9731c6b005da36e7d3037
naru <tus.naru@gmail.com>
2017-06-13 17:28:03 +0900

    Fix to create view snapshot

--- Sources/UIView+SnapshotImage.swift
+++ Sources/UIView+SnapshotImage.swift
@@ -15,9 +15,7 @@ public extension UIView {
         let size: CGSize = CGSize(width: floor(self.frame.size.width), height: floor(self.frame.size.height))
         UIGraphicsBeginImageContextWithOptions(size, true, 0.0)
         
-        if let context: CGContext = UIGraphicsGetCurrentContext() {
-            self.layer.render(in: context)
-        }
+        self.drawHierarchy(in: self.bounds, afterScreenUpdates: true)
         let snapshot: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
         
         UIGraphicsEndImageContext()
